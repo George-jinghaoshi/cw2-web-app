be67e0d2858c4ca64f4a4d6661112bc39ae5a917
George-jinghaoshi <146245432+George-jinghaoshi@users.noreply.github.com>
2024-12-05 21:55:33 +0000

    Initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# cw2-web-app
---
+++ app/models.py
@@ -0,0 +1,30 @@
+from app import db
+from flask_login import UserMixin
+
+# User Model
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+
+# Product Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    image = db.Column(db.String(1000), nullable=False)
+
+    def __repr__(self):
+        return f"Product('{self.name}', '{self.price}')"
+
+# Cart Model
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False, default=1)
+
+    def __repr__(self):
+        return f"Cart(User ID: '{self.user_id}', Product ID: '{self.product_id}', Quantity: '{self.quantity}')"
